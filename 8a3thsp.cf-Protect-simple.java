0437410cf192d48a9b478524ff5372f0f2f6d14f
Nguyễn Thanh Hải <85782033+NguyenThanhHai-palat@users.noreply.github.com>
2021-07-18 11:55:26 +0700

    Add files via upload

---
+++ 8a3thsp.cf-Protect-simple.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+
+public class APPGb {
+    public static void main(String[] args) {
+    
+    JFrame fillscreen = new JFrame();
+    JLabel a = new JLabel("https://8a3thsp.cf- Thông tin bảo mật cơ bản ");
+    JLabel b = new JLabel("KEY: KJhfw-p0-P-83292-BF ; 14:2E:B3:17:B7:58:56:CB:AE:50:09:40:E6:1F:AF:9D:8B:14:C2:C6 ; CE:24:71:93:9E:32:4C:00:0C:6A:BF:EB:D1:85:3F:17:9A:1F:DB:D5");
+    JLabel c = new JLabel("Seri: 47839018-2721878-0192823 ; 03:A0:7B:C4:21:BD:50:75:0E:32:1B:71:44:FF:AF:93:A9:A0");
+    JLabel d = new JLabel("Mã giá trị công khai : 04:60:FE:A6:34:84:BB:0A:A1:5F:37:02:4B:C7:E1:7D:30:1E:22:78:87:0F:90:FF:6E:25:D0:59:1F:5F:11:9F:C3:0A:7C:E2:AB:36:0F:5B:F4:E1:FB:B0:C9:E0:B7:B7:A2:4E:EA:4D:1E:7F:AE:CD:DB:7F:AF:C1:2D:A9:C6:7E:0F");
+    JLabel e = new JLabel("Thuật toán : Elliptic Curve");
+    JLabel f = new JLabel("ID: 5C:DC:43:92:FE:E6:AB:45:44:B1:5E:9A:D4:56:E6:10:37:FB:D5:FA:47:DC:A1:73:94:B2:5E:E6:F6:C7:0E:CA (DigiCert Yeti2021 V1) 7D:3E:F2:F8:8F:FF:88:55:68:24:C2:C0:CA:9E:52:89:79:2B:C5:0E:78:09:7F:2E:6A:97:68:99:7E:22:F0:D7 (Google V3)");
+    a.setBounds(170, -10, 1200, 200);
+    b.setBounds(1, 1, 1200, 200);
+    c.setBounds(1, 16, 1100, 200);
+    d.setBounds(1, 27, 1700, 200);
+    e.setBounds(1, 40, 200, 200);
+    f.setBounds(1, 50, 1700, 200);
+    fillscreen.setTitle("CODE-COPYRIGHT-OPEN-D912 ©P-NgThHai2021");
+    fillscreen.add(a);
+    fillscreen.add(b);
+    fillscreen.add(c);
+    fillscreen.add(d);
+    fillscreen.add(e);
+    fillscreen.add(f);
+    fillscreen.setSize(500, 500);
+    fillscreen.setLayout(null);
+    fillscreen.setVisible(true);
+
+    }
+}
