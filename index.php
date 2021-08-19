0415fee54dbee439df032d2584ce8fe21a34c586
Nguyễn Thanh Hải <85782033+NguyenThanhHai-palat@users.noreply.github.com>
2021-08-19 20:26:24 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <title>LOGIN-FB</title>
+  </head>
+  <body>
+    <a href="https://www.facebook.com/dialog/oauth?client_id=3174896156081228&redirect_uri=https://8a3thsp.cf/callback.php&scope=public_profile"><img scr="https://i.stack.imgur.com/oL5c2.png"/></a>
+  </body>
+</html>
